04fb222e271857808b104e610a371ea5f5bebd98
NueXini <34617094+NueXini@users.noreply.github.com>
2021-09-30 22:05:18 +0800

    fix libcryptopp

--- main.sh
+++ main.sh
@@ -166,7 +166,7 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/network/services/samba
 rm -rf .svn
 rm -rf ./default-settings
 
-sed -i 's#http://www.cryptopp.com/#https://github.com/NueXini/Update_NueXini_Packages/raw/master/other/#g' ./libcryptopp/Makefile
+sed -i 's#http://www.cryptopp.com/#https://www.cryptopp.com/#g' ./libcryptopp/Makefile
 sed -i 's#wpad-openssl#wpad-basic-wolfssl#g' ./luci-app-easymesh/Makefile
 sed -i 's/default y/default n/g' ./luci-app-diskman/Makefile
 sed -i 's/default y/default n/g' ./luci-app-rclone/Makefile
