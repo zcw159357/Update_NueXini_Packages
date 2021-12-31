66babfeb23a45e7315253b908699b69ff1d574b7
NueXini <34617094+NueXini@users.noreply.github.com>
2021-12-31 16:40:26 +0800

    add luci-app-qos-gargoyle

--- main.sh
+++ main.sh
@@ -5,11 +5,11 @@
 ####################################################################################################
 
 # 石像鬼QOS
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06/package/emortal/luci-app-qos-gargoyle ./Gargoyle-Qos/luci-app-qos-gargoyle
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06/package/emortal/gargoyle-firewall-util ./Gargoyle-Qos/gargoyle-firewall-util
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06/package/emortal/qos-gargoyle ./Gargoyle-Qos/qos-gargoyle
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06/package/emortal/libiptbwctl ./Gargoyle-Qos/libiptbwctl
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06/package/emortal/libericstools ./Gargoyle-Qos/libericstools
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/luci-app-qos-gargoyle ./Gargoyle-Qos/luci-app-qos-gargoyle
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/gargoyle-firewall-util ./Gargoyle-Qos/gargoyle-firewall-util
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/qos-gargoyle ./Gargoyle-Qos/qos-gargoyle
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/libiptbwctl ./Gargoyle-Qos/libiptbwctl
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/emortal/libericstools ./Gargoyle-Qos/libericstools
 rm -rf ./Gargoyle-Qos/*/.svn
 ####################################################################################################
 
