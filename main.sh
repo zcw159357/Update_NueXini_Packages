bb1ae79293c79b4a29285bd649b98cb16f6587eb
NueXini <34617094+NueXini@users.noreply.github.com>
2022-01-09 01:46:49 +0800

    add luci-app-nezha

--- main.sh
+++ main.sh
@@ -4,6 +4,12 @@
 
 ####################################################################################################
 
+# luci-app-nezha 哪吒监控
+svn co https://github.com/Erope/openwrt_nezha/trunk/luci-app-nezha ./luci-app-nezha
+svn co https://github.com/Erope/openwrt_nezha/trunk/openwrt-nezha ./openwrt-nezha
+rm -rf .svn
+####################################################################################################
+
 # mosdns
 svn co https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns ./luci-app-mosdns
 svn co https://github.com/QiuSimons/openwrt-mos/trunk/mosdns ./mosdns
