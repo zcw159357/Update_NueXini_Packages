2d6c78e0b761760977b6b47c017b12a41b14ca8a
NueXini <34617094+NueXini@users.noreply.github.com>
2022-03-01 09:19:22 +0800

    add luci-app-v2raya

--- main.sh
+++ main.sh
@@ -4,6 +4,10 @@
 
 ####################################################################################################
 
+# luci-app-v2raya
+git clone https://github.com/zxlhhyccc/luci-app-v2raya -b main --single-branch --depth=1 ./luci-app-v2raya
+####################################################################################################
+
 # luci-app-nezha 哪吒监控
 svn co https://github.com/Erope/openwrt_nezha/trunk/luci-app-nezha ./luci-app-nezha
 svn co https://github.com/Erope/openwrt_nezha/trunk/openwrt-nezha ./openwrt-nezha
